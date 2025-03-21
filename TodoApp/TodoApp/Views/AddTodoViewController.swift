0b03b7586fca467bc00b0a49a5407c05a1ddf3c9
hiereit-dev <https://github.com/hiereit-dev>
2025-03-21 11:48:41 +0900

    할일 추가화면 UI 개선

--- TodoApp/TodoApp/Views/AddTodoViewController.swift
+++ TodoApp/TodoApp/Views/AddTodoViewController.swift
@@ -13,6 +13,14 @@ class AddTodoViewController: UIViewController {
         let textField = UITextField()
         textField.placeholder = "할일을 입력하세요."
         textField.borderStyle = .roundedRect
+        textField.layer.borderColor = UIColor.systemGray.cgColor
+        textField.layer.borderWidth = 1
+        textField.layer.cornerRadius = 8
+            
+        let paddingView = UIView(frame: CGRect(x: 0, y: 0, width: 8, height: textField.frame.height))
+        textField.leftView = paddingView
+        textField.leftViewMode = .always
+        
         return textField
     }()
     
@@ -21,6 +29,15 @@ class AddTodoViewController: UIViewController {
         label.font = .systemFont(ofSize: 14)
         label.isHidden = true // 초기에 안보이게 처리
         return label
+    }()
+    
+    let doneButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("등록", for: .normal)
+        button.backgroundColor = .systemBlue  // 배경색을 설정하여 버튼이 보이게 함
+        button.layer.cornerRadius = 8
+        button.clipsToBounds = true
+        return button
     }()
     
     var navigationDataDelegate: NavigationDelegate?
@@ -36,10 +53,6 @@ class AddTodoViewController: UIViewController {
     private func configureNavigation() {
         title = "할일 추가"
         navigationController?.navigationBar.prefersLargeTitles = false
-        
-        navigationItem.rightBarButtonItem = UIBarButtonItem(
-            barButtonSystemItem: .done, target: self, action: #selector(saveItem)
-        )
     }
     
     private func setupUI() {
@@ -47,20 +60,29 @@ class AddTodoViewController: UIViewController {
         
         textField.translatesAutoresizingMaskIntoConstraints = false
         messageLabel.translatesAutoresizingMaskIntoConstraints = false
+        doneButton.translatesAutoresizingMaskIntoConstraints = false
         
         view.addSubview(textField)
         view.addSubview(messageLabel)
+        view.addSubview(doneButton)
         
         NSLayoutConstraint.activate([
+            doneButton.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -16),
+            doneButton.widthAnchor.constraint(equalToConstant: 60),
+            doneButton.heightAnchor.constraint(equalToConstant: 44),
+            doneButton.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 24),
+            
             textField.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 16),
-            textField.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -16),
-            textField.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 24),
+            textField.trailingAnchor.constraint(equalTo: doneButton.leadingAnchor, constant: -16),
+            textField.centerYAnchor.constraint(equalTo: doneButton.centerYAnchor),
             textField.heightAnchor.constraint(equalToConstant: 44),
             
             messageLabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 16),
             messageLabel.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -16),
             messageLabel.topAnchor.constraint(equalTo: textField.bottomAnchor, constant: 8)
         ])
+        
+        doneButton.addTarget(self, action: #selector(saveItem), for: .touchUpInside)
     }
     
     @objc func saveItem() {
